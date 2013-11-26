528c220155556c1a7bc9b579da160772fd045d67
Dmitry Ponyatov <dponyatov@gmail.com>
2013-11-26 12:55:38 +0400

    + POST7
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ POST7/fetch.py
@@ -0,0 +1,14 @@
+import os,sys,time,re
+
+D=r'C:\Program Files\Adem71\ncm\POSTPR'
+
+for i in os.listdir(D):
+    if re.match(r'.+(0999|1210)\..+',i):
+        print D,i,'->',i
+        S=open(r'%s\%s'%(D,i),'rb')
+        T=open(i,'wb')
+        T.write(S.read())
+        S.close()
+        T.close()
+
+    
