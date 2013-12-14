fd6a39fe3016ce969499a32b7f05c86476981c27
Dmitry Ponyatov <dponyatov@gmail.com>
2013-12-14 16:24:38 +0400

    * posts 6000 & 0888
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- POST7/fetch.py
+++ POST7/fetch.py
@@ -2,6 +2,8 @@ import os,sys,time,re
 
 D=r'C:\Program Files\Adem71\ncm\POSTPR'
 
+#os.system('git checkout *.pst')
+
 for i in os.listdir(D):
     if re.match(r'^(.+(0888|0999|6000|1210).+|STANKI\.SKR)$',i):
         print D,i,'->',i
